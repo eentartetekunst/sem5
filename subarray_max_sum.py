c39a4cbe6fe4ae9a8a9f203c524cd6a8631fd23d
eentartetekunst <60537368+eentartetekunst@users.noreply.github.com>
2021-02-07 18:02:04 +0300

    Update subarray_max_sum.py

--- subarray_max_sum.py
+++ subarray_max_sum.py
@@ -16,12 +16,11 @@ for k in range(len(S)):
         local_max = S[k]
         r_border = k
 
-j = r_border
 l_border = 0
 
-for j in range(r_border, 0, -1):
-    if S[j] == S[j-1] + seq[j]:
-        j -= 1
+for l_border in range(r_border, 0, -1):
+    if S[l_border] == S[l_border-1] + seq[l_border]:
+        l_border -= 1
     else:
         break
 
